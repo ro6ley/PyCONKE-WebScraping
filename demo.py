0f5bf271584f28f13aaa4ec14e80f6dc8b55f95a
Robley Gori <robleyadrian@gmail.com>
2017-09-18 10:30:11 +0300

    Demo of a Github Scrapper using Beautiful Soup

---
+++ demo.py
@@ -0,0 +1,32 @@
+# import the library to query a website
+import urllib2
+
+# import beautiful soup too
+from bs4 import BeautifulSoup
+
+# specify the url to scrap
+urlToScrap = 'https://github.com/Mochualex4?tab=repositories'
+
+# Query the web page
+page = urllib2.urlopen(urlToScrap)
+
+# parse the html file
+soup = BeautifulSoup(page, 'html.parser')
+
+# to view the source
+# print(soup.prettify)
+
+# Find main content
+main_content = soup.find('div', {'id': 'user-repositories-list'})
+
+# Extract list of repositories
+list_of_repos = main_content.findAll('li')
+
+# Extract each repos details
+for repo in list_of_repos:
+    print('Repository name: ' + repo.a.string)
+    print('Link: http://github.com' + repo.a.get('href'))
+    if repo.p and repo.p.string:
+        print("Description: " + repo.p.string)
+    else:
+        print("No description available for this repository")
